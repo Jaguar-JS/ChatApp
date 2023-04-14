abb5fbd003ae109909aac45ed4beb6de091013ea
Oleg <117010302+Jaguar-JS@users.noreply.github.com>
2023-04-14 11:15:56 +0300

    Initial commit

---
+++ src/component/ChatList.js
@@ -0,0 +1,201 @@
+// Імпортуємо потрібні технології
+import React from "react";
+import styled, { css } from "styled-components";
+// Підключаємо іконку непрочитаного повідомлення
+import { ReactComponent as UnreadIcon } from "../file/chat/unread.svg";
+import { ReactComponent as ReadIcon } from "../file/chat/read.svg";
+
+// Генеруємо компонент "Список чатів"
+export default function ChatList({ list }) {
+  return <List>{list.map(ChatItem)}</List>;
+}
+// Створюємо елемент "Cписок", який відповідає за секцію списку чатів
+const List = styled.div`
+  /* Задаємо колір фону */
+  background-color: #fff;
+  /* Включаємо режим grid, щоб всі елементи списку ставали вертикально */
+  display: grid;
+  /* Задаємо сірі рамки знизу та зверху списку */
+  border-top: solid #d7d7d7 1px;
+  border-bottom: solid #d7d7d7 1px;
+`;
+
+// Генеруємо компонент "Один Чат", в якому ми отримуємо дані одного чату та виводимо їх
+function ChatItem({
+  photo,
+  message,
+  name,
+  id,
+
+  messageAmount,
+  notifyOff,
+  isRead,
+  isUnread,
+  time,
+  user,
+  hashtag
+}) {
+  return (
+    <React.Fragment key={id}>
+      <Item>
+        <Image src={photo} />
+        <Content>
+          <Row>
+            <InfoColumn>
+              <Name>{name}</Name>
+              <User>{user}</User>
+              <Message>{message}</Message>
+              <Hashtag>{hashtag}</Hashtag>
+            </InfoColumn>
+            <StatusColumn>
+              <Time>{time}</Time>
+
+              {messageAmount && (
+                <MessageAmount notifyOff={notifyOff}>
+                  {messageAmount}
+                </MessageAmount>
+              )}
+              {isRead && (
+                <Icon>
+                  <ReadIcon />
+                </Icon>
+              )}
+              {isUnread && (
+                <Icon>
+                  <UnreadIcon />
+                </Icon>
+              )}
+            </StatusColumn>
+          </Row>
+        </Content>
+      </Item>
+    </React.Fragment>
+  );
+}
+const Hashtag = styled.div`
+  font-size: 14px;
+  color: #037ee5;
+`;
+
+const User = styled.div`
+  font-size: 14px;
+  color: #111;
+`;
+
+const Time = styled.div`
+  font-size: 14px;
+  color: #8e8e93;
+`;
+// Елемент "Повідомлення"
+const Message = styled.div`
+  font-size: 14px;
+  color: #8e8e93;
+`;
+// Елемент "Ім'я"
+const Name = styled.div`
+  font-size: 16px;
+  color: #111;
+`;
+
+// ======================================
+// Елемент "Іконка"
+const Icon = styled.div`
+  height: 42px;
+  width: 42px;
+
+  & > svg {
+    height: 24px;
+    width: 24px;
+  }
+`;
+
+const MessageAmount = styled.div`
+  font-size: 12px;
+  color: #fff;
+  background-color: #037ee5;
+  padding: 2px 10px;
+  border-radius: 15px;
+
+  ${({ notifyOff }) => {
+    if (notifyOff === true) {
+      return css`
+        background-color: #8e8e93;
+      `;
+    }
+  }}
+`;
+
+// ======================================
+// Далі йдуть елементи, які відповідають за відображення даних одного чату в потрібній структурі
+// Елемент колонки статусів
+const StatusColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+// Елементи колонки з головної інформації
+const InfoColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+// Елемент, який відповідає за рядок, в якому виводять колонки
+const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+`;
+
+// =========================================
+
+// Елемент "Картинка" чату
+const Image = styled.img`
+  /* Задаємо ширину та висоту */
+  width: 60px;
+  height: 60px;
+  /* Відступ праворуч, щоб текст опції не притискався до картинки */
+  margin-right: 16px;
+  /* Відступ зверху та знизу, щоб картинка не налазила на рамку карточки опції */
+  margin-top: 8px;
+  margin-bottom: 8px;
+  /* Робимо закруглення країв */
+  border-radius: 50%;
+`;
+
+// Елемент "Контент", в якому знаходяться основні дані про чат
+const Content = styled.div`
+  /* Задаємо текст  на всю вільну ширину */
+  width: 100%;
+  /* Відступ зверху та знизу, щоб елемент не налазив на рамку карточки чату */
+  padding-top: 8px;
+  padding-bottom: 8px;
+  /* Включаємо режим верстки flex, робимо відображення елементів вертикально */
+  display: flex;
+  flex-direction: column;
+  /* Робимо вертикальний відступ між елементами */
+  gap: 2px;
+`;
+
+// Елемент "Один чат", який відповідає за відображення карточки одного чату
+const Item = styled.div`
+  /* Включаємо режим flex */
+  display: flex;
+  /* Відступ справа та зліва, щоб елементи не налазили на краї */
+  padding-left: 16px;
+  padding-right: 16px;
+  /* Робимо, щоб всі елементи "Заголовок" окрім останнього */
+  /* мали знизу сіру лінію */
+  &:not(:last-of-type) > ${Content} {
+    border-bottom: 1px solid #d7d7d7;
+  }
+  /* Задаємо можливість робити анімацію */
+  transition: opacity 0.7s;
+  /* Рибимо анімацію при наведені курсором мишки*/
+  &:hover {
+    /* Виставляємо прозорість на 50% */
+    opacity: 0.5;
+    /* Робимо інший дизайн курсору */
+    cursor: pointer;
+  }
+`;
---
+++ src/page/Chats.js
@@ -0,0 +1,80 @@
+// Підключаємо потрібні технології
+import React from "react";
+
+// ============================================
+
+// Імпортуємо компонент "Шапка"
+import Header from "../component/Header";
+// Імпортуємо компонент "Меню"
+import Menu from "../component/Menu";
+// Імпортуємо компонент "Сторінка"
+import Page from "../component/Page";
+// Імпортуємо компонент "Пусто"
+import ChatList from "../component/ChatList";
+
+// ============================================
+// Імпортуємо файли потрібних картинок
+
+import CatPhoto from "../file/photo/cat.png";
+import DadPhoto from "../file/photo/dad-ivan.png";
+import SonPhoto from "../file/photo/son-ilya.png";
+import MaPhoto from "../file/photo/mother-nata.png";
+// ============================================
+// Генеруємо константи, в яких тримаємо дані про список чатів
+
+const CHATS_LIST = [
+  {
+    id: 1,
+    photo: DadPhoto,
+    name: "Dad",
+    message: "And where is my Kitsya-matsitsa?",
+    time: "8:15",
+    isRead: true
+  },
+  {
+    id: 2,
+    photo: CatPhoto,
+    name: "Про котів",
+    message: "Я і є твоя киця-мациця!",
+
+    user: "Dad",
+    time: "8:20",
+    isUnread: true,
+    hashtag: "#Cats"
+  },
+  {
+    id: 3,
+    photo: MaPhoto,
+    name: "Ma",
+    message: "Я люблю пити молоко",
+    messageAmount: 3,
+    time: "9:00",
+    isRead: false
+  },
+  {
+    id: 4,
+    photo: SonPhoto,
+    name: "Son",
+    message: "Я люблю пити молоко",
+    time: "9:15",
+    messageAmount: 3,
+    notifiOff: false
+  }
+];
+
+// ============================================
+// Генеруємо сторінку "Чати"
+
+export default function Chats() {
+  return (
+    <Page>
+      {/* В title передаємо текст заголовка сторінки */}
+      <Header title="Чати" />
+      <div>
+        <ChatList list={CHATS_LIST} />
+      </div>
+      {/* В activePage передаємо посилання поточної сторінки */}
+      <Menu activePage="/chats" />
+    </Page>
+  );
+}
